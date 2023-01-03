c7c34b0cc89d1e2f2ce4b89b340642263ae5ea2a
Jānis Alunāns <j.alunans@gmail.com>
2023-01-04 00:31:54 +0200

    Show game's state in the header, add reset button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,13 @@
-function Header({ score, hiScore }) {
+let headerValues = {lost: "You lost the game ...",
+                    won: "You won the game!",
+                    playing: "The Memory Game"}
+
+function Header({ score, hiScore, reset, gameState }) {
   return (
     <header>
-      <h1>Memory game</h1>
+      <h1 className={gameState}>{headerValues[gameState]}</h1>
       <h2>Current score: {score} High score: {hiScore}</h2>
+      <button onClick={reset}>Reset</button>
     </header>
   )
 }
