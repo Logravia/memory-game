af3c18991be90d40b2d94f21d1edbc3541efa9c4
Jānis Alunāns <j.alunans@gmail.com>
2023-01-03 23:25:25 +0200

    Add rudimentary functionality of the game

--- src/App.js
+++ src/App.js
@@ -4,18 +4,46 @@ import images from './images'
 import Card from './card'
 import Cardbox from './components/card-box'
 import Header from './components/header'
+import {shuffle} from './helper.js'
 
 function App() {
-  const generateCards = ()=> images.map(img=>new Card(img))
 
-  let [cards, setCards] = useState(generateCards);
+  let [cards, setCards] = useState(generateCards(images));
   let [score, setScore] = useState(0);
   let [hiScore, setHiScore] = useState(0);
-
+  let [gameState, setGameState] = useState("start");
 
   const handleCardClick = id => {
+    let clickedCard = cards.find(card=>card.id == id);
 
+    if (clickedCard.clicked){
+      gameOver()
+      return;
+    } else {
+      incrementScore();
+      clickedCard.clicked = true;
+    }
+
+    let newCards = [...cards]
+    setCards(shuffle(newCards));
   }
+
+  function generateCards (images) {
+    return images.map(img=>new Card(img))
+  }
+
+  function incrementScore() {
+    let curScore = score + 1
+    setScore(curScore);
+    if (curScore > hiScore) { setHiScore(curScore) }
+  }
+
+  function gameOver(){
+    setScore(0);
+    setCards(generateCards(images))
+    setGameState("Lost")
+  }
+
 
   return (
     <div className="App">
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,10 @@
-function Header ({score, hiScore}) {
-  <header>
-    <h1>Memory game</h1>
-    <h2>Current score: {score} High score: {hiScore}</h2>
-  </header>
+function Header({ score, hiScore }) {
+  return (
+    <header>
+      <h1>Memory game</h1>
+      <h2>Current score: {score} High score: {hiScore}</h2>
+    </header>
+  )
 }
 
 export default Header
