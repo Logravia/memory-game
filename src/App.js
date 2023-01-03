d8165b80fa26a3c5f2499acef9131f5b760d82fe
Jānis Alunāns <j.alunans@gmail.com>
2023-01-04 00:32:39 +0200

    Add victory, loss and playing states

--- src/App.js
+++ src/App.js
@@ -4,51 +4,73 @@ import images from './images'
 import Card from './card'
 import Cardbox from './components/card-box'
 import Header from './components/header'
-import {shuffle} from './helper.js'
+import { shuffle } from './helper.js'
 
 function App() {
 
   let [cards, setCards] = useState(generateCards(images));
   let [score, setScore] = useState(0);
   let [hiScore, setHiScore] = useState(0);
-  let [gameState, setGameState] = useState("start");
+  let [gameState, setGameState] = useState("playing");
 
   const handleCardClick = id => {
-    let clickedCard = cards.find(card=>card.id == id);
+    let clickedCard = cards.find(card => card.id == id);
 
-    if (clickedCard.clicked){
-      gameOver()
-      return;
+    if (clickedCard.clicked) {
+      gameOver(); return;
     } else {
-      incrementScore();
       clickedCard.clicked = true;
+      let curScore = incrementScore();
+
+      if (victory(curScore)) { gameWon() }
     }
 
     let newCards = [...cards]
     setCards(shuffle(newCards));
   }
 
-  function generateCards (images) {
-    return images.map(img=>new Card(img))
+  function generateCards(images) {
+    return images.map(img => new Card(img))
   }
 
   function incrementScore() {
     let curScore = score + 1
     setScore(curScore);
     if (curScore > hiScore) { setHiScore(curScore) }
+    return curScore;
   }
 
-  function gameOver(){
+  function reset() {
     setScore(0);
-    setCards(generateCards(images))
-    setGameState("Lost")
+    resetCards()
+    setGameState("playing")
   }
 
+  function gameOver() {
+    setGameState("lost")
+    lockCards();
+  }
+
+  function gameWon() {
+    setGameState("won")
+  }
+
+  function victory(curScore) {
+    return curScore === cards.length
+  }
+
+  function resetCards(){
+    cards.forEach(card=>card.clicked = false);
+  }
+
+  function lockCards(){
+    cards.forEach(card=>card.clicked = true);
+  }
 
   return (
     <div className="App">
-      <Header score={score} hiScore={hiScore}/>
-      <Cardbox cards={cards} noteClick={handleCardClick}></Cardbox>
+      <Header score={score} hiScore={hiScore} gameState={gameState} reset={reset}/>
+      <Cardbox cards={cards} noteClick={handleCardClick}/>
     </div>
   );
 }
